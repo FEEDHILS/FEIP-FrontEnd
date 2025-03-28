45c367a328def7d1a72c7e6fcfbc2dc929a2ae19
des0ager <dmitriy1350077@gmail.com>
2025-03-28 15:56:11 +1000

    3 ДЗ

---
+++ header.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="header">
+        <h1>Header</h1>
+    </div>
+</template>
+
+<style lang="scss">
+@use './style.scss' as main;
+
+.header {
+    display: flex;
+    background: main.gradient(lightblue, green);
+    height: 20vh;
+    align-items: center;
+    padding-left: 5%;
+    color: whitesmoke;
+}
+</style>
+
+<script>
+
+</script>
--- js/msg_change.js
+++
@@ -1,9 +0,0 @@
-const messages = ["Hello World!", "Hello FEFU!"];
-let current = 0;
-const button = document.querySelector(".button");
-
-button.addEventListener("click", () => {
-    const text = document.querySelector(".message");
-    current = (current+1) % messages.length;
-    text.textContent = messages[current];
-});
